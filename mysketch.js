e234c85dce83feacbcaff4e03480576fb26e43a6
jefmeijvis <32873147+jefmeijvis@users.noreply.github.com>
2018-03-16 15:54:20 +0100

    Update mysketch.js

--- mysketch.js
+++ mysketch.js
@@ -3,6 +3,17 @@ var w = 0.95*window.innerWidth;
 var h = 0.95*window.innerHeight;
 console.log("Script started V10");
 
+function setup()
+{
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+	background('#457b9d');
+}
+
+
 function test()
 {
 console.log("Test");
@@ -30,7 +41,6 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
 ctx.rect(100,100,1000,1000);
 
-ctx.text("Jef",300,300);
 ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
 ctx.fillRect(100, 100, 200, 200);
 ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
