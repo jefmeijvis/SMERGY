759839ec04cf34a67ddd3da05f3ecc53bae53438
jefmeijvis <32873147+jefmeijvis@users.noreply.github.com>
2018-05-28 11:02:46 +0200

    Add files via upload

--- scripts/energyComparisson.js
+++ scripts/energyComparisson.js
@@ -14,33 +14,62 @@ function drawEnergyComparisson()
 
 function drawComp(player)
 {
+    var alpha = 0.007315;
+    var beta = 0.01490;
+    var g = 9.81;
+    var mass = 60.45;
+
+
+  textAlign(LEFT);
   var x = w/2;
   if (player == 1)
   {
-    x = x/4;
+    x = x/2;
   }
   if (player == 2)
   {
-    x = 3*x/4;
+    x = 3*x/2;
   }
+
+  fill(360);
+  rect(x,140,300,400);
+  rect(x,140,-300,400);
+
   fill('#99cc67');
+  rect(x,140,300,-30);
+  rect(x,140,-300,-30);
+
+
+
   if (player == 1)
   {
-    text("Name: " + nameOne,x,200);
-    text("Time: " + getTimeStringFromMillis(timeOne),x,250);
+    var avgSpeed =  round(3.6 * 200/(timeOne/1000));
+    var avgPower = (alpha*mass*g+beta*(avgSpeed,2))*avgSpeed/3.6;
+
+    text("Name: " + nameOne,x-280,180);
+    text("Time: " + getTimeStringFromMillis(timeOne),x-280,220);
+    text("Average Speed: " + avgSpeed + ' Km/u',x-280,260);
+    text("Average Power: " + round(avgPower) + " W" ,x-280,300);
+    text("Total Energy: " + round(avgPower * timeOne/1000) + " J",x-280,340);
     var power =  getPowerFromSpeed(speedOne);
-    text("Closest match: " + getNameFromSpeed(speedOne),x,300);
-    icon = getIcon(power);
-    image(icon,x,350,icon.width/(icon.width/80),icon.height/(icon.height/80));
+    text("Closest match: " + getNameFromSpeed(speedOne),x-280,440);
+    icon = getIcon(speedOne);
+    image(icon,x,490,icon.width/(icon.width/80),icon.height/(icon.height/80));
   }
   if (player == 2)
   {
-    text("Name: " + nameTwo,x,200);
-    text("Time: " + getTimeStringFromMillis(timeTwo),x,250);
-    var power =  getPowerFromSpeed(speedOne);
-    text("Closest match: " + getNameFromSpeed(speedOne),x,300);
-    icon = getIcon(power);
-    image(icon,x,350,icon.width/(icon.width/80),icon.height/(icon.height/80));
+    var avgSpeed =  round(3.6 * 200/(timeTwo/1000));
+    var avgPower = (alpha*mass*g+beta*(avgSpeed,2))*avgSpeed/3.6;
+
+    text("Name: " + nameTwo,x-280,180);
+    text("Time: " + getTimeStringFromMillis(timeTwo),x-280,220);
+    text("Average Speed: " + avgSpeed + ' Km/u',x-280,260);
+    text("Average Power: " + round(avgPower) + " W" ,x-280,300);
+    text("Total Energy: " + round(avgPower * timeTwo/1000) + " J",x-280,340);
+    var power =  getPowerFromSpeed(speedTwo);
+    text("Closest match: " + getNameFromSpeed(speedTwo),x-280,440);
+    icon = getIcon(speedTwo);
+    image(icon,x,490,icon.width/(icon.width/80),icon.height/(icon.height/80));
   }
 
 }
